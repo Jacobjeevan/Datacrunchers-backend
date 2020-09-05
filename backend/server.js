f2a2c40de3b1eecaf43f9a9b691e8b50d63a272d
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-05 12:56:50 -0500

    jwt initial setup
    - Initial jwt setup (to app to use jwt)

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,35 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 require("dotenv").config();
+const jwt = require("express-jwt");
+const jwtAuthz = require("express-jwt-authz");
+const jwksRsa = require("jwks-rsa");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+// Authentication middleware. When used, the
+// Access Token must exist and be verified against
+// the Auth0 JSON Web Key Set
+const checkJwt = jwt({
+  // Dynamically provide a signing key
+  // based on the kid in the header and
+  // the signing keys provided by the JWKS endpoint.
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: process.env.jwksUri,
+  }),
+
+  // Validate the audience and the issuer.
+  audience: process.env.audience,
+  issuer: process.env.issuer,
+  algorithms: ["RS256"],
+});
 
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, {
