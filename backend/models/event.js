eae231960651466feee69fd8b81fc8de49a0cf69
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-05 01:16:38 -0500

    adding event schema

---
+++ backend/models/event.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: date,
+    required: true,
+  },
+});
+
+const Event = mongoose.model("Event", eventSchema);
+
+module.exports = Event;
