c052568d2b9b7e82a41baf1429d73c583af46a61
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-07 08:34:01 -0500

    Image upload
    - Added backend support for image upload and retrieval for Officer route
    	- The route would upload/retrieve image to/from Amazon S3 buckets

--- backend/routes/officer.js
+++ backend/routes/officer.js
@@ -1,18 +1,42 @@
 const router = require("express").Router();
 let Officer = require("../models/officer");
+const multer = require("multer");
+const AWS = require("aws-sdk");
+const fs = require("fs");
+require("dotenv").config();
 
-router.route("/add").post((req, res) => {
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+const upload = multer({ storage: storage });
+
+AWS.config.update({
+  accessKeyId: process.env.Access_Key_ID,
+  secretAccessKey: process.env.Secret_Access_Key,
+  region: process.env.bucketregionregion,
+});
+
+const s3 = new AWS.S3();
+
+router.route("/add", upload.single("headshot")).post((req, res) => {
   const name = req.body.name;
   const title = req.body.title;
   const description = req.body.description;
   const contact = req.body.contact;
-  const imageUrl = req.body.imageUrl;
+  const imageName = req.file.filename;
+  const uploadRes = uploadFile(req.file.path, imageName);
+  if (uploadRes === false) {
+    res.status(400).json("Image upload Error");
+  }
   const newOfficer = new Officer({
     name,
     title,
     description,
     contact,
-    imageUrl,
+    imageName,
   });
   newOfficer
     .save()
@@ -20,16 +44,71 @@ router.route("/add").post((req, res) => {
     .catch((err) => res.status(400).json("Error: " + err));
 });
 
+function uploadFile(source, targetName) {
+  fs.readFile(source, function (err, filedata) {
+    if (!err) {
+      const putParams = {
+        Bucket: process.env.bucketregion,
+        Key: targetName,
+        Body: filedata,
+      };
+      s3.putObject(putParams, function (err, data) {
+        if (err) {
+          return false;
+        } else {
+          fs.unlink(source);
+          return true;
+        }
+      });
+    } else {
+      console.log({ err: err });
+      return false;
+    }
+  });
+}
+
 router.route("/").get((req, res) => {
   Officer.find()
-    .then((officers) => res.json(officers))
+    .then((officers) => {
+      officers.map(function callback(officer) {
+        officer.image = retrieveFile(officer.imageName);
+        if (officer.image === false) {
+          res.status(400).json("Image retrieval Error");
+        } else {
+          return officer;
+        }
+      });
+
+      res.json(officers);
+    })
     .catch((err) => res.status(400).json("Error: " + err));
 });
+
+function retrieveFile(filename) {
+  const getParams = {
+    Bucket: process.env.bucketregion,
+    Key: filename,
+  };
+
+  s3.getObject(getParams, function (err, data) {
+    if (err) {
+      return false;
+    } else {
+      return data.Body;
+    }
+  });
+}
 
 router.route("/:id").get((req, res) => {
   let id = req.params.id;
   Officer.findById(id)
-    .then((officer) => res.json(officer))
+    .then((officer) => {
+      officer.image = retrieveFile(officer.imageName);
+      if (officer.image === false) {
+        res.status(400).json("Image retrieval Error");
+      }
+      res.json(officer);
+    })
     .catch((err) => res.status(400).json("Officer Fetch Error: " + err));
 });
 
@@ -40,7 +119,7 @@ router.route("/update/:id").post((req, res) => {
       officer.title = req.body.title;
       officer.description = req.body.description;
       officer.contact = req.body.contact;
-      officer.imageUrl = req.body.imageUrl;
+      officer.imageName = req.body.imageName;
       officer
         .save()
         .then(() => res.json("Officer Updated"))
