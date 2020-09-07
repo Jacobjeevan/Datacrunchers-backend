1637c3922e060a9ebe2a3cbdf859d42c7df8976e
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-07 08:35:41 -0500

    added Routers to server

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,23 @@ require("dotenv").config();
 const jwt = require("express-jwt");
 const jwtAuthz = require("express-jwt-authz");
 const jwksRsa = require("jwks-rsa");
+const officerRouter = require("./routes/officer");
+const eventRouter = require("./routes/event");
+const projectRouter = require("./routes/project");
+const resourceRouter = require("./routes/resource");
+const careerRouter = require("./routes/career");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/officers", officerRouter);
+app.use("/projects", projectRouter);
+app.use("/events", eventRouter);
+app.use("/resources", resourceRouter);
+app.use("/careers", careerRouter);
 
 // Authentication middleware. When used, the
 // Access Token must exist and be verified against
