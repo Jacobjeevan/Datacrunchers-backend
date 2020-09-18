f96ab23c043783f7190d77b573d4ed631eefb033
Jacobjeevan <jeevan.jacobjohn@gmail.com>
2020-09-18 06:56:21 -0500

    Installed and added helmet package for http headers

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const helmet = require("helmet");
 require("dotenv").config();
 
 const officerRouter = require("./routes/officer");
@@ -12,6 +13,7 @@ const careerRouter = require("./routes/career");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 
